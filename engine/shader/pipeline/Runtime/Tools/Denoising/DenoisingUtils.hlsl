f66c585ef25b9318b0af491db772307b0fa50912
alienity <578409434@qq.com>
2024-08-29 03:47:49 +0800

    调整融合范围

--- engine/shader/pipeline/Runtime/Tools/Denoising/DenoisingUtils.hlsl
+++ engine/shader/pipeline/Runtime/Tools/Denoising/DenoisingUtils.hlsl
@@ -1,12 +1,15 @@
 
-#define MAX_REPROJECTION_DISTANCE 0.1
+#define MAX_REPROJECTION_DISTANCE 0.5
 #define MAX_PIXEL_TOLERANCE 4
 #define PROJECTION_EPSILON 0.000001
 
 float ComputeMaxReprojectionWorldRadius(FrameUniforms frameUniform, float3 positionWS, float3 normalWS, float pixelSpreadAngleTangent, float maxDistance, float pixelTolerance)
 {
-    const float3 viewWS = GetWorldSpaceNormalizeViewDir(frameUniform, positionWS);
-    float parallelPixelFootPrint = pixelSpreadAngleTangent * length(positionWS);
+    const float3 positionVS = GetWorldSpaceViewDir(frameUniform, positionWS);
+    const float3 viewWS = normalize(positionVS);
+    // const float3 viewWS = GetWorldSpaceNormalizeViewDir(frameUniform, positionWS);
+    // float parallelPixelFootPrint = pixelSpreadAngleTangent * length(positionWS);
+    float parallelPixelFootPrint = pixelSpreadAngleTangent * length(positionVS);
     float realPixelFootPrint = parallelPixelFootPrint / max(abs(dot(normalWS, viewWS)), PROJECTION_EPSILON);
     return max(maxDistance, realPixelFootPrint * pixelTolerance);
 }
